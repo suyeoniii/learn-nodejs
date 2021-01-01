aac0107f88eb171961e560e84b8a7a5f694aa524
ssy4230@naver.com <ssy4230@naver.com>
2021-01-02 00:00:50 +0900

    ch 3.4

---
+++ nextTick.js
@@ -0,0 +1,17 @@
+setImmediate(() => {
+    console.log('immediate');
+});
+process.nextTick(() => {
+    console.log('nextTick');
+});
+setTimeout(() => {
+    console.log('timeout');
+}, 0);
+Promise.resolve().then(() => console.log('promise'));
+
+/*console
+nextTick
+promise
+timeout
+immediate
+*/
