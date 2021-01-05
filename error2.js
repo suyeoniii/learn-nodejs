eac96a3bf30ff12d48928bd6651c920fd390e6c5
ssy4230@naver.com <ssy4230@naver.com>
2021-01-05 22:14:50 +0900

    ch 4.3

---
+++ error2.js
@@ -0,0 +1,9 @@
+const fs = require('fs').promises;
+
+setInterval(() => {
+    fs.unlink('./abcdefg.js', (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+}, 1000);
